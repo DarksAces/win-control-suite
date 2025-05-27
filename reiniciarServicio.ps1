fe44aec84ded6059b354ec09f3b3d66f191073fb
Daniel <166744714+DarksAces@users.noreply.github.com>
2025-05-27 11:21:49 +0200

    Update reiniciarServicio.ps1

--- reiniciarServicio.ps1
+++ reiniciarServicio.ps1
@@ -4,9 +4,9 @@ $flagOK = "C:\1\green.flag"
 $flagError = "C:\1\red.flag"
 
 try {
-    Stop-Service -Name "Zabbix Agent 2" -ErrorAction Stop
+    Stop-Service -Name "Servicio" -ErrorAction Stop
     Start-Sleep -Seconds 5
-    Start-Service -Name "Zabbix Agent 2" -ErrorAction Stop
+    Start-Service -Name "Servicio" -ErrorAction Stop
     Start-Sleep -Seconds 2
     New-Item -Path $flagOK -ItemType File -Force | Out-Null
 }
